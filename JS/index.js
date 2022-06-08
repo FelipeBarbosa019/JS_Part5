d2254d4e3c868759e7858ffedd8b39b4e8066bf8
Felipe Barbosa <felipefb2@hotmail.com>
2022-06-08 13:57:08 -0300

    JS_Part5

---
+++ JS/index.js
@@ -0,0 +1,49 @@
+const btn = document.querySelector ('#submit')
+const text = document.querySelector ('h3')
+let check
+
+btn.addEventListener ("click", function(e) {
+    e.preventDefault()
+    const num = parseFloat(document.getElementById ('num').value.replace(',','.'))
+    check = Number. isInteger(num)
+
+    if ((isNaN(num) || check===false) || ((num<0) || (num>10))) {
+        alert (`Insira um número inteiro de 0 a 10.`)
+    }
+    else {
+        switch (num){
+            case 0:
+                text.textContent = 'O numúmero digitado foi zero.'
+                break
+            case 1:
+                text.textContent = 'O numúmero digitado foi um.'
+                break
+            case 2:
+                text.textContent = 'O numúmero digitado foi dois.'
+                break
+            case 3:
+                text.textContent = 'O numúmero digitado foi três.'
+                break
+            case 4:
+                text.textContent = 'O numúmero digitado foi quatro.'
+                break
+            case 5:
+                text.textContent = 'O numúmero digitado foi cinco.'
+                break
+            case 6:
+                text.textContent = 'O numúmero digitado foi seis.'
+                break
+            case 7:
+                text.textContent = 'O numúmero digitado foi sete.'
+                break
+            case 8:
+                text.textContent = 'O numúmero digitado foi oito.'
+                break
+            case 9:
+                text.textContent = 'O numúmero digitado foi nove.'
+                break
+            case 10:
+                text.textContent = 'O numúmero digitado foi dez.'
+        }
+    }
+});
